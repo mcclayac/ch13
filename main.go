5b078a60c11e260284f91fc8d94c9534ba393a19
Anthony McClay <anthony.mcclay@gmail.com>
2026-06-29 11:42:02 -0500

    io.reader and io.writer

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
 
 func main() {
 
@@ -9,5 +14,40 @@ func main() {
 		fmt.Println("Hello World")
 		fmt.Println("Hello World")
 
+	}
+	countingLettersFunction()
+}
+
+func countingLettersFunction() {
+
+	fmt.Println("counting letters function")
+
+	s := "The quick brown fox jumps over the lazy dog"
+	sReader := strings.NewReader(s)
+	counts, err := countLetters(sReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(counts)
+}
+
+func countLetters(r io.Reader) (map[string]int, error) {
+
+	fmt.Println("Counting letters")
+	buff := make([]byte, 2048)
+	out := map[string]int{}
+	for {
+		n, err := r.Read(buff)
+		for _, b := range buff[:n] {
+			if (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
+				out[string(b)]++
+			}
+		}
+		if err == io.EOF {
+			return out, nil
+		}
+		if err != nil {
+			return nil, err
+		}
 	}
 }
